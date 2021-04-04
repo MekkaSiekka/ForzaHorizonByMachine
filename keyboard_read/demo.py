2a699cf8e3bddd4b78126da107a0514c76a4a7af
MekkaSiekka <jiangxiao961123@gmail.com>
2021-04-04 17:15:40 +0800

    [feat] use pynput and dict to keep track of currently pressed key

--- keyboard_read/demo.py
+++ keyboard_read/demo.py
@@ -1,20 +1,30 @@
 
 from pynput import keyboard
 
+keyboard_status = {}
+
+
 def on_press(key):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
+        
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
-
+    if key.char in ['w','a','s','d']:
+        keyboard_status[key.char] = 1
+    print(keyboard_status)
+    
 def on_release(key):
     print('{0} released'.format(
         key))
+    if key.char in ['w','a','s','d']:
+        keyboard_status[key.char] = 0
     if key == keyboard.Key.esc:
         # Stop listener
         return False
+    print(keyboard_status)
 
 
 # Collect events until released
